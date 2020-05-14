3c3a14fea618b2f168fb9371567b50322ebc48b9
Samuel Oechsler <samuel@oechsler.it>
2020-05-14 13:35:44 +0200

    Extract "getFrequentRenterPoints" method

--- src/Movie.java
+++ src/Movie.java
@@ -29,6 +29,14 @@ public class Movie {
         }
         return result;
     }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        // add bonus for a two day new release rental
+        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
+            return 2;
+        return 1;
+    }
+
     public int getPriceCode() {
         return priceCode;
     }
--- src/Rental.java
+++ src/Rental.java
@@ -19,10 +19,7 @@ class Rental {
         return movie.getCharge(daysRented);
     }
 
-    public int getFrequentRenterPoints() {
-        // add bonus for a two day new release rental
-        if ((movie.getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
-            return 2;
-        return 1;
+    int getFrequentRenterPoints() {
+        return movie.getFrequentRenterPoints(daysRented);
     }
 }
